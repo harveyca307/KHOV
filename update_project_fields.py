c142d64280f06d6cf091cbcc3822c6a31c5ec4cc
Chad Harvey <harveyca@gmail.com>
2022-11-04 11:01:48 -0500

    11.04 Morning Upload

--- update_project_fields.py
+++ update_project_fields.py
@@ -12,6 +12,8 @@ Options:
 """
 
 from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
+import multiprocessing
 
 import asana
 import asana.error as ae
@@ -25,13 +27,12 @@ APP_NAME = "CIP-UpdateProjectFields"
 APP_VERSION = '1.0'
 
 
-def update_proj(pat: str, project_id: str, project: str, custom_fields: dict) -> str:
-    logger.info(f"{APP_NAME} - Updating Project: {project} custom_fields: {custom_fields}")
+def update_proj(pat: str, project_id: str, project: str, custom_fields: dict) -> None:
+    logger.info(f"{APP_NAME} - Updating Project: {project}:{project_id} custom_fields: {custom_fields}")
     client = asana.Client.access_token(pat)
     client.LOG_ASANA_CHANGE_WARNINGS = False
     try:
         client.projects.update_project(project_id, custom_fields)
-        return f'Done with {project}'
     except ae.AsanaError as e:
         logger.info(e)
 
@@ -49,25 +50,24 @@ def main(fields: dict) -> None:
 
     jobs = []
     _dict = {}
-    _entries = {}
+    _args = dict()
     for project in fields:
+        _entries = dict()
         _project = fields[project]['project_id']
         _cust = fields[project]["custom_fields"]
         for key, value in _cust.items():
             _dict[key] = value
         _entries['custom_fields'] = _dict
-        t = Thread(target=update_proj, args=(pat, _project, project, _entries))
-        jobs.append(t)
-    [t.start() for t in jobs]
-    [t.join() for t in jobs]
+        process = multiprocessing.Process(target=update_proj, args=(pat, _project, project, _entries))
+        process.start()
 
 
 if __name__ == "__main__":
     cmd_args = docopt(__doc__, version=f"{APP_NAME}, Version: {APP_VERSION}")
-    logger.info(f"Starting process {APP_NAME}, .  CMD_ARGS={cmd_args}")
+    logger.info("\n")
+    logger.info(f"Starting process {APP_NAME}, File sent: {cmd_args['<file>']}")
     _file = cmd_args.get("<file>")
     stream = open(_file, 'r')
     _yml = yaml.load(stream, Loader=yaml.FullLoader)
     main(fields=_yml)
     stream.close()
-    logger.info(f"{APP_NAME} - Process finished")
