285e74997a3269b9982b7a3be0ccc051504ad138
Chad Harvey <harveyca@gmail.com>
2022-11-17 09:30:15 -0600

    11.17 Morning Upload

--- get_project_tasks_async.py
+++ get_project_tasks_async.py
@@ -13,6 +13,7 @@ Options:
 import asyncio
 import os
 import time
+from pathlib import Path
 
 import aiohttp
 import pandas as pd
@@ -21,7 +22,7 @@ from TM1py import TM1Service
 from TM1py.Exceptions import TM1pyException
 from docopt import docopt
 
-from Utilities import asana_client, DB, PySecrets
+from Utilities import DB, PySecrets, asana_tasks
 from baseLogger import logger, application_path
 
 APP_NAME = "CIP-GetProjectTasks"
@@ -71,51 +72,56 @@ def get_tm1_config(conf: dict) -> dict:
     return _return
 
 
-async def main(projects: list, pat: str, config: dict):
+async def main(projects: list, pat: str, config: dict, output_file: str) -> None:
     async with aiohttp.ClientSession() as session:
         token = pat
 
         task_items = []
+        proj_dict = {}
         for community, gid in projects:
             task_items.append(
-                asana_client(
-                    "GET", f"/projects/{gid}/tasks", session=session, token=token, params="opt_fields=name,"
-                                                                                          "due_on,"
-                                                                                          "start_on,"
-                                                                                          "custom_fields,"
-                                                                                          "projects"
+                asana_tasks(
+                     "GET", f"/projects/{gid}/tasks", session=session, token=token, params="opt_fields=name,"
+                                                                                           "due_on,"
+                                                                                           "start_on,"
+                                                                                           "custom_fields"
                 )
             )
+            proj_dict[gid] = community
 
         array_of_responses = await asyncio.gather(*task_items)
         output = []
         for response in array_of_responses:
+            _community = response['community']
+            _community = proj_dict[_community]
             for item in response['data']:
                 _name = item['name']
                 _due = item['due_on']
                 _start = item['start_on']
-                _gid = item['gid']
-                _community = item['projects']
                 _milestone = item['custom_fields'][0]['name']
                 _milestone_val = item['custom_fields'][0]['display_value']
                 _team = item['custom_fields'][1]['name']
                 _team_val = item['custom_fields'][1]['display_value']
                 _actual = item['custom_fields'][2]['name']
-                _actual_dt = item['custom_fields'][2]['display_value']
+                _actual_dt = str(item['custom_fields'][2]['display_value'])[0:10]
                 _line = item['custom_fields'][3]['name']
                 _line_id = item['custom_fields'][3]['display_value']
+                if _due is not None:
+                    output.append([_community, _name, 'Due On', _due])
+                if _start is not None:
+                    output.append(([_community, _name, 'Start On', _start]))
                 if _milestone_val is not None:
-                    output.append([_community[0]['gid'], _name, _milestone, _milestone_val])
+                    output.append([_community, _name, _milestone, _milestone_val])
                 if _team_val is not None:
-                    output.append([_community[0]['gid'], _name, _team, _team_val])
-                if _actual_dt is not None:
-                    output.append([_community[0]['gid'], _name, _actual, _actual_dt])
+                    output.append([_community, _name, _team, _team_val])
+                if _actual_dt is not None and _actual_dt != 'None':
+                    output.append([_community, _name, _actual, _actual_dt])
                 if _line_id is not None:
-                    output.append([_community[0]['gid'], _name, _line, _line_id])
+                    output.append([_community, _name, _line, _line_id])
 
     df = pd.DataFrame(output)
     df.columns = ['Community', 'Task', 'Field', 'Value']
-    df.to_csv(os.path.join(application_path, 'test.csv'), index=False)
+    df.to_csv(output_file, index=False)
     try:
         with TM1Service(**config) as tm1:
             print(tm1.server.get_product_version())
@@ -134,8 +140,11 @@ if __name__ == '__main__':
     _token = retrieve_pat()
     _projects = retrieve_project_list(fields=_yml)
     _config = get_tm1_config(conf=_yml['Config'])
+    _output = fr"{_yml['Config']['output_directory']}"
+    # _output = Path(_output)
+    _output = os.path.join(_output, "CIP-ProjectTasks.csv")
     try:
-        asyncio.run(main(projects=_projects, pat=_token, config=_config))
+        asyncio.run(main(projects=_projects, pat=_token, config=_config, output_file=_output))
     except KeyboardInterrupt:
         pass
     end = time.perf_counter()
